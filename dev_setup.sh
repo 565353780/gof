9361d530a1c947141687c3b2c853c529f546f3e0
chLi <565353780@qq.com>
2026-01-21 11:23:20 +0800

    use common tetra-triangulation package as default

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,3 +1,7 @@
+cd ..
+git clone git@github.com:565353780/tetra-triangulation.git
+git clone --depth 1 https://github.com/camenduru/simple-knn.git
+
 conda install cmake conda-forge::gmp conda-forge::cgal -y
 
 pip3 install torch torchvision torchaudio \
@@ -6,13 +10,11 @@ pip3 install torch torchvision torchaudio \
 pip install plyfile tqdm open3d ninja GPUtil \
   opencv-python lpips trimesh einops
 
-cd ./submodules/diff-gaussian-rasterization
-python setup.py install
+cd tetra-triangulation
+./dev_setup.sh
 
 cd ../simple-knn
 python setup.py install
 
-cd ../submodules/tetra-triangulation
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .
-make -j
-pip install .
+cd ../gof/submodules/diff-gaussian-rasterization
+python setup.py install
